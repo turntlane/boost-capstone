23be6755468c03ca4e8a4b29a9834b7611739279
Jake Alexander <imjakeal123@gmail.com>
2021-10-17 19:41:10 -0600

    axios call to make list of videos

--- src/Components/Skill Select/SkillSelect.js
+++ src/Components/Skill Select/SkillSelect.js
@@ -1,24 +1,32 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import keys from "../../key/accessKey";
+import { auth, db, logout } from "../Firebase/firebase";
+
+
+
 function SkillSelect(props) {
   const [value, setValue] = useState("");
   const [skill, setSkill] = useState("");
   const [videos, setVideos] = useState([]);
 
-  const handleSubmit = () => {
-    axios
+  const handleSubmit = async () => {
+    await axios
       .get(
         `https://youtube.googleapis.com/youtube/v3/search?part=snippet&q=${value} ${skill} tutorial&key=${keys.ytKey}`
       )
       .then((res) => {
-        setVideos(res.data.items[0].snippet.title);
+        // console.log(res.data.items)
+        res.data.items.map(item => console.log(item.id.videoId))
+        setVideos(res.data.items.map(item => item.id.videoId))
+        // setVideos(res.data.items[0].snippet.thumbnails.default.url);
+        // setVideos(res.data.items.id.videoId)
       });
   };
 
   return (
     <div>
-        <div  className="dashboard-header">Welcome, {props.name}!</div>
+        <div  className="dashboard-header">Welcome, {db.name}!</div>
       <select
         value="Current Skill Level"
         onChange={(e) => setValue(e.target.value)}
@@ -32,7 +40,14 @@ function SkillSelect(props) {
 
       <input placeholder="Skill" onChange={(e) => setSkill(e.target.value)} />
       <button onClick={handleSubmit}>Get call</button>
-      <h1>{videos}</h1>
+      {/* <iframe src={`https://www.youtube.com/embed/${videos}`}></iframe> */}
+      {videos.map((video, i) => 
+      <li key={i}>
+        <iframe src={`https://www.youtube.com/embed/${video}`}></iframe>
+      </li>
+        // <iframe key={video.videoId} src={`https://www.youtube.com/embed/${videos}`}></iframe>
+
+        )}
     </div>
   );
 }
